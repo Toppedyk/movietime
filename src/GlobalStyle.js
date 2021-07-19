59f130a343fbbd73be58193e2f2dc431ee6f0656
Toppedyk <Tayloroppedyk@yahoo.com>
2021-07-19 11:06:39 -0600

    body styling

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -12,6 +12,21 @@ export const GlobalStyle = createGlobalStyle`
   --fontMed: 1.2rem;
   --fontSmall: 1rem;
 }
+*{
+  box-sizing: border-box;
+  font-family: 'Abel, sans-serif';
+}
+
+body{
+  margin: 0;
+  padding:0;
+
+  h1{
+    font-size: 2rem;
+    font-weight: 600;
+    color: var(--white) 
+  }
+}
 
 
 
