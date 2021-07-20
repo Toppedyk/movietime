a531892e074a716a8634e945b76fba17b7c5c7c6
Toppedyk <Tayloroppedyk@yahoo.com>
2021-07-20 14:33:20 -0600

    first logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMBDLogo  from '../../images/tmdb_logo.svg';
-import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
+import { Wrapper, Content, LogoImg, TMBDLogoImg} from "./Header.styles";
 
 const Header = () => (
   <Wrapper>
     <Content>
       <LogoImg src={RMDBLogo} alt="rmdb-logo"/>
-      <TMDBLogoImg src={TMBDLogo} alt="tmbd-logo"/>
+      <TMBDLogoImg src={TMBDLogo} alt="tmbd-logo"/>
     </Content>
   </Wrapper>
 );
