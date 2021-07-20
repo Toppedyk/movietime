843535d573c6bfcfa34664fedc52d30d10b47ca0
Toppedyk <Tayloroppedyk@yahoo.com>
2021-07-20 14:24:48 -0600

    logos in header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,3 +3,14 @@ import React from 'react';
 import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMBDLogoImg  from '../../images/tmdb_logo.svg';
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
+
+const Header = () => (
+  <Wrapper>
+    <Content>
+      <LogoImg src={RMDBLogo} alt="rmdb-logo"/>
+      <TMDBLogoImg src={TMDBLogoImg} alt="tmbd-logo"/>
+    </Content>
+  </Wrapper>
+);
+
+export default Header;
